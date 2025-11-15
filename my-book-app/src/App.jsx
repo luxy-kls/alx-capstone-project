47c2954944eab1f1687b6ea56a0f886045354bf5
luxy-kls <sackeykeyman@gmail.com>
2025-11-15 10:49:46 +0000

    Fixed Bug

--- my-book-app/src/App.jsx
+++ my-book-app/src/App.jsx
@@ -2,3 +2,33 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Browse from './pages/Browse';
+import BookDetails from './pages/BookDetails';
+import SearchResults from './pages/SearchResults';
+import MyLibrary from './pages/MyLibrary';
+
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/browse" element={<Browse />} />
+            <Route path="/books/:id" element={<BookDetails />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/library" element={<MyLibrary />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
---
+++ my-book-app/src/components /Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../components/Footer.css';
+
+function Footer(){
+  
+  return (
+    <footer className='footer'>
+      <div className='footer-container' >
+        <p> &copy;2025 | &reg; KLS Bookhub | Made By Keyman Luxy Sackey </p>
+        <p>Data from Google Book API</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
