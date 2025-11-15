b5a439297f28c4d0b268afd4f9f31fec799c0e8c
* <sackeykeyman@gmail.com>
2025-11-15 12:40:23 +0000

    fixed and tested app

---
+++ my-book-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(){
+  return (
+    <footer className='footer'>
+      <div className='footer-container'>
+        <p>&copy;2025 | &reg; KLS Bookhub | Made By Keyman Luxy Sackey</p>
+        <p>Data from Google Book API</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
