be7f2983622bda08bf421ae8dc2e6eec27da916c
luxy-kls <sackeykeyman@gmail.com>
2025-11-26 14:37:05 +0000

    Added files for Loading Spinner animation

---
+++ my-book-app/src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LoadingSpinner.css'
+
+function LoadingSpinner(){
+  return(
+ <div className='loading-spinner'>
+   <div className= 'spinner'>
+     <p>Loading books...</p>
+   </div>
+ </div> 
+ )
+}
+
+
+export default LoadingSpinner;
