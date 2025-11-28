1680f1636bde4dd8d3e33176c22dee0e2c260081
luxy-kls <sackeykeyman@gmail.com>
2025-11-28 13:38:41 +0000

    imported components and set state variables

--- my-book-app/src/pages/Home.jsx
+++ my-book-app/src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import '../styles/Home.css'
+import React from 'react';
+import '../styles/Home.css';
+import { useState, useEffect } from 'react';
+import getTrendingBooks from '../utils/api';
+import searchByCategory from '../utils/api';
+import BookGrid from '../components/BookGrid';
+import LoadingSpinner from '../components/LoadingSpinner';
+import ErrorMessage from '../components/ErrorMessage';
 
 
 function Home(){
-  return(
-    <div>
-      <h1>Welcome To Lux Reader</h1>
-      <p>Homepage will is under construction🏗🏗🏗</p>
-    </div>
-  )
+  // setting up state variables
+  const [trendingBooks, setTrendingBooks] = useState([]);
+  const [fictionBooks, setFictionBooks] = useState([]);
+  const loading = useState( true );
+  const error = useState(null);
+  
+  
+  
 }
 
 export default Home;
