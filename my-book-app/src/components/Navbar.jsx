4db0e89141b230178d0807980ab4d78ceebf7716
luxy-kls <sackeykeyman@gmail.com>
2025-12-03 20:05:15 +0000

    Improved search functionality

--- my-book-app/src/components/Navbar.jsx
+++ my-book-app/src/components/Navbar.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React ,{ useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css'
 
+
 function Navbar() {
-  return (
+  const [searchQuery ,setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    e.pdefaulDefault();
+    
+    if(searchQuery.trim()) {
+      navigate(`/search?q=${searchQuery}`);
+      setSearchQuery(''); // to clear after search has completed
+    }
+  }
+ return (
     <nav className='navbar'>
       <div className="navbar-container">
         <Link className="navbar-logo" to="/">Bookhub</Link>
+        
         <ul className="navbar-menu">
           <Link to="/">Home</Link>
           <Link to="/browse">Browse</Link>
           <Link to="/library">My Library</Link>
         </ul>
+        
         <div className="navbar-search">
-          <input type="text" placeholder="search" className="search-input" />
+          <form onSubmit={handleSearch}>
+            <input 
+              type="text" 
+              placeholder="Search books..." 
+              className="search-input"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </form>
         </div>
       </div>
     </nav>
