37bec9f7cb31c7db276936dff1b10efbd0961375
luxy-kls <sackeykeyman@gmail.com>
2025-12-04 16:30:11 +0000

    Added functions to make search results appear

--- my-book-app/src/pages/SearchResults.jsx
+++ my-book-app/src/pages/SearchResults.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { useSearchParams } from 'react-router-dom';
 import '../styles/SearchResults.css'
+import ErrorMessage from '../components/ErrorMessage';
+import LoadingSpinner from '../components/LoadingSpinner';
+import BookGrid from '../components/BookGrid';
+import { searchBooks } from '../utils/api';
+
 
 function SearchResults(){
+  // setting states
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading]= useState(true);
+  const [error, setError] = useState(null);
+  
   const [searchParams] = useSearchParams();
   const query = searchParams.get('q')
+  
+  useEffect(()=>{
+    const fetchSearchResults = async ()=>{
+      try {
+        setLoading(true);
+        
+        const results = await searchBooks(query)
+        setBooks(result.items || []);
+        
+      } catch(err){
+        setError(err.message) 
+      } finally {
+        setLoading(false);
+        fetchSearchResults();
+      }
+    }
+  }, [query]);
+  
+  //Adding conditional statements
+  if (loading) return <LoadingSpinner />
+  if (error) return <ErrorMessage message={ err } />
+  
  return (
    <div className="search-results">
-     <h1>Results from your search...</h1>
-     <p>Search for {query}</p>
-     <p>Under construction 🏗</p>
+     <h1>Search Results</h1>
+    <p>Found {books.length} books for "{query}"</p>
+    {books.length === 0 ? (
+  <p>No books found</p>
+) : (
+  <BookGrid books={books} />
+)}
    </div>
  );
 }
